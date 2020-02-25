cdd5714862262a87ae0d4f6634627d344e2f53ae
Samuel Jaros <sjaros@luc.edu>
2020-02-25 17:05:28 -0600

    Part 3 finished

--- runSleuth.R
+++ runSleuth.R
@@ -2,7 +2,7 @@ library(sleuth)
 library(dplyr)
 
 # Show sleuth where kallisto results are stored
-results = data.frame("sample"=c("SRR5660030.1","SRR5660033.1","SRR5660044.1","SRR5660045.1"), "condition"=c("2dpi","6dpi","2dpi","6dpi"), "path"=c("results/SRR5660030.1","results/SRR5660033.1","results/SRR5660044.1","results/SRR5660045.1"))
+results = data.frame("sample"=c("SRR5660030.1","SRR5660033.1","SRR5660044.1","SRR5660045.1"), "condition"=c("2dpi","6dpi","2dpi","6dpi"), "path"=c("results/SRR5660030.1","results/SRR5660033.1","results/SRR5660044.1","results/SRR5660045.1"), stringsAsFactors=F)
 
 print(results)
 
@@ -12,7 +12,7 @@ so = sleuth_prep(results)
 # Perform differential expression analysis
 so = sleuth_fit(so, ~condition, "full")
 so = sleuth_fit(so, ~1, "reduced")
-so = sleuth_fit(so, "reduced", "full")
+so = sleuth_lrt(so, "reduced", "full")
 
 # Extract significant results
 sleuthResults = sleuth_results(so, "reduced:full", "lrt", show_all=F)
@@ -20,4 +20,4 @@ sleuthSignificant = dplyr::filter(sleuthResults, qval <= 0.05) %>% dplyr::arrang
 sleuthLog = dplyr::select(sleuthSignificant, target_id, test_stat, pval, qval)
 
 # Write results to log
-write.table(sleuthLog, file="miniProject.log", quote=F, row.names=F)
+write.table(sleuthLog, file="miniProject.log", quote=F, row.names=F, sep="\t")
