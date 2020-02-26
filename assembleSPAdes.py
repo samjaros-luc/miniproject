1390eb37ad217301a7ab5becc56c463a725667d3
Sam Jaros <61028727+samjaros-luc@users.noreply.github.com>
2020-02-26 01:50:10 -0600

    Part 5 complete

--- assembleSPAdes.py
+++ assembleSPAdes.py
@@ -1,17 +1,15 @@
 import os
 
-# Open log file
 log = open("miniProject.log","a")
 
-
 ## Part 4: Run spades on Bowtie output ##
-command = "spades.py --disable-gzip-output --only-assembler "
+command = "spades.py --disable-gzip-output --only-assembler " # beginning of command
 index = 1
 for file in os.listdir("data"):
-    # assemble spades command, no need to zip output, only assemble the sequences, index defines each pair of reads
+    # create input section of spades command
     command += "--pe"+str(index)+"-1 results/"+file+"/"+file+".1.fastq --pe"+str(index)+"-2 results/"+file+"/"+file+".2.fastq "
     index += 1
-command += "-o results/SPAdes"
+command += "-o results/SPAdes" # output section of spades command
 os.system(command) # run the command
 log.write(command+"\n") # log the command
 log.close()
